9dcd3a8d1c1308eed42e4eaac70057df40ae499f
MegaThorx <git@merx.dev>
2025-03-08 12:15:27 +0100

    Add topics controller with GetAll method

---
+++ src/McCardz.API/Controllers/TopicsController.cs
@@ -0,0 +1,24 @@
+﻿using McCardz.Domain.Models;
+using McCardz.Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace McCardz.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TopicsController : ControllerBase
+{
+    private readonly ITopicRepository _topicRepository;
+
+    public TopicsController(ITopicRepository topicRepository)
+    {
+        _topicRepository = topicRepository;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Topic>))]
+    public async Task<ActionResult<IEnumerable<Topic>>> GetAll()
+    {
+        return Ok(await _topicRepository.FindAllAsync());
+    }
+}
