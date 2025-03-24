318f019c5447c9fa8e8ecc144a4ba9b92135a588
MegaThorx <git@merx.dev>
2025-03-24 12:14:09 +0100

    Add identity migrations

--- src/McCardz.Infrastructure/Data/Migrations/20250324105704_AddIdentity.Designer.cs
+++ src/McCardz.Infrastructure/Data/Migrations/20250324105704_AddIdentity.Designer.cs
@@ -4,6 +4,7 @@ using McCardz.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace McCardz.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250324105704_AddIdentity")]
+    partial class AddIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
