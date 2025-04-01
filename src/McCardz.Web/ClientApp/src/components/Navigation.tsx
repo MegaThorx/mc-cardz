d16e14b60ac4283d8af5da8d01226b4fb228a7af
MegaThorx <git@merx.dev>
2025-04-01 10:54:34 +0200

    Remove bottom border from navigation component

--- src/McCardz.Web/ClientApp/src/components/Navigation.tsx
+++ src/McCardz.Web/ClientApp/src/components/Navigation.tsx
@@ -12,7 +12,7 @@ export default function () {
 
     return <div className="gradient-bg">
         <div className="container">
-            <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+            <header className="d-flex flex-wrap justify-content-center py-3 mb-4">
                 <NavLink to="/"
                          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
                     <span className="fs-3 bold text-white">McCardz</span>
